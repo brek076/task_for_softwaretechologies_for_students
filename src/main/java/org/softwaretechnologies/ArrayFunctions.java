60ad0af479d18a6f42f31f5e8dedd68e35752a08
brek076 <38405777+brek076@users.noreply.github.com>
2024-10-11 11:25:12 +0300

    Done this task

--- src/main/java/org/softwaretechnologies/ArrayFunctions.java
+++ src/main/java/org/softwaretechnologies/ArrayFunctions.java
@@ -3,6 +3,7 @@ package org.softwaretechnologies;
 public final class ArrayFunctions {
 
     private ArrayFunctions() {
+
     }
 
     /**
@@ -10,7 +11,11 @@ public final class ArrayFunctions {
      * @param array массив, который будет перевернут.
      */
     public static void reverse(int[] array) {
-        // TODO: реализуйте вышеуказанную функцию
+        for(int i = 0; i< array.length / 2; i++){
+            int temp = array[i];
+            array[i] = array[array.length - i - 1];
+            array[array.length - i - 1] = temp;
+        }
     }
 
     /**
@@ -24,6 +29,16 @@ public final class ArrayFunctions {
      * @param matrix матрица, в которой столбцы будут заменены на строки.
      */
     public static void rotateMatrix(int[][] matrix) {
-        // TODO: реализуйте вышеуказанную функцию
+        if(matrix[0].length != matrix.length){
+            System.out.println("Матрица не квадратная");
+        }
+
+        for (int i = 0; i < matrix.length ; i++){
+            for (int j = i + 1; j < matrix.length ; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
     }
 }
